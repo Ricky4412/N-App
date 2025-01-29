9bc4553b1305c6cb0fa8b1764a989c2f153f7c8a
Ricky4412 <124883161+Ricky4412@users.noreply.github.com>
2025-01-29 17:20:45 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'https://app-frontend-5bs4-h3a15j06p-enricos-projects-bf5d6f99.vercel.app/', 
+    origin: 'https://app-frontend-5bs4.vercel.app/', 
     methods: ['GET', 'POST'],
     credentials: true,
   },
