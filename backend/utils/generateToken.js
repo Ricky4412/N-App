7956153e24c9ce0c3ed1fc78b7e1b1ce22fcd3fa
Ricky4412 <124883161+Ricky4412@users.noreply.github.com>
2025-02-08 04:56:28 +0000

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 // Function to generate a JWT token
-const generateToken = (id) => {
+const generateToken = (id, expiresIn = '30d') => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined in environment variables');
   }
 
-  // Sign the token with the user ID and secret, and set the expiration time to 30 days
+  // Sign the token with the user ID and secret, and set the expiration time
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
+    expiresIn,
   });
 };
 
