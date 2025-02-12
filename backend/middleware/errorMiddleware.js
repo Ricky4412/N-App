2c1c051f63601ba4ee42c3b575d71654010578ca
Ricky4412 <124883161+Ricky4412@users.noreply.github.com>
2025-02-12 16:37:44 +0000

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -7,4 +7,10 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-module.exports = { errorHandler };
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+module.exports = { errorHandler, notFound };
